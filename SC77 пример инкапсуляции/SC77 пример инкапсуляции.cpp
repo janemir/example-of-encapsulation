710c340a8ae9233a972d58b28271885797201643
janemir <144587149+janemir@users.noreply.github.com>
2024-01-29 13:00:03 +0300

    Добавьте файлы проекта.

---
+++ SC77 пример инкапсуляции/SC77 пример инкапсуляции.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+class CoffeeGrinder {
+private:
+    bool CheckVoltage() { // "checking the voltage with a coffee grinder"
+        return true; // encoded
+    }
+public:
+    void Start() { // "vjuhhh" - the coffee grinder is working; "beep beep" - a signal about an inappropriate voltage
+        (CheckVoltage()) ? (cout << "vjuhhh" << endl) : (cout << "beep beep" << endl);
+    }
+};
+
+int main() {
+    CoffeeGrinder a;
+    a.Start();
+    return 0;
+}
